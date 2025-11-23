088842058c73e55b03e82e21bcdcb4561ef9006e
ReyAndrey21 <bitoleanu.eugen.f5a@student.ucv.ro>
2025-11-23 23:33:49 +0200

    Added DbContext

---
+++ PetMarket/Data/PetMarketDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace PetMarket.Models
+
+{
+    public class PetMarketDbContext : IdentityDbContext<User>
+    {
+        public PetMarketDbContext(DbContextOptions<PetMarketDbContext> dbContextOptions) :
+            base(dbContextOptions) 
+        { }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<UserAddress> UserAddresses { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Favorites> Favorites { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+        public DbSet<ShippingMethod> ShippingMethods { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<PaymentStatus> PaymentStatuses { get; set; }
+
+    }
+}
